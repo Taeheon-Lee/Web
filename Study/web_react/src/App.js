c659725a1d76a3f472f7583d1d707e7bd9afd86f
Taeheon-Lee <taeheon714@gmail.com>
2020-04-12 17:05:14 -0700

    add fuction of update and delete to react

--- Study/web_react/src/App.js
+++ Study/web_react/src/App.js
@@ -1,15 +1,19 @@
 import TOC from "./components/TOC.js"
 import Subject from "./components/Subject.js"
-import Content from "./components/Content.js"
+import ReadContent from "./components/ReadContent.js"
+import CreateContent from "./components/CreateContent.js"
+import UpdateContent from "./components/UpdateContent.js"
+import Control from "./components/Control.js"
 import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.max_content_id = 3;
     this.state = {
-      mode: 'read',
-      selected_content_id: 2,
+      mode: 'welcome',
+      selected_content_id: 1,
       subject:{title:'WEB', sub:'world wide web!'},
       welcome:{title:'Welcome', desc:'Hello, React!!'},
       contents:[
@@ -19,24 +23,74 @@ class App extends Component {
       ]
     }
   }
-  render() {
-    var _title, _desc = null;
-    if (this.state.mode === 'welcome') {
-      _title = this.state.welcome.title;
-      _desc = this.state.welcome.desc;
-    }
-    else if (this.state.mode === 'read') {
-      var i = 0;
+
+  getReadContent() {
+    var i = 0;
       while(i < this.state.contents.length) {
         var data = this.state.contents[i];
         if (data.id === this.state.selected_content_id) {
-          _title = data.title;
-          _desc = data.desc;
-          break;
+          return data
+          // break;
         }
         i = i + 1;
       }
+  }
+
+  getContent() {
+    var _title, _desc, _article = null;
+
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
     }
+
+    else if (this.state.mode === 'read') {
+      var _content = this.getReadContent();
+      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>;
+    }
+
+    else if (this.state.mode === 'create') {
+      _article = <CreateContent onSubmit={function(_title, _desc) {
+        // add content to this.state.contents
+        this.max_content_id++;
+        // this.state.contents.push(
+        //   {id: this.max_content_id, title: _title, desc: _desc}
+        // );
+        var _contents = this.state.contents.concat(
+          {id: this.max_content_id, title: _title, desc: _desc}
+        ) 
+        this.setState({
+          contents: _contents,
+          mode: 'read',
+          selected_content_id: this.max_content_id
+        });
+      }.bind(this)}></CreateContent>;
+    }
+
+    else if (this.state.mode === 'update') {
+      _content = this.getReadContent();
+      _article = <UpdateContent data={_content} onSubmit={
+        function(_id, _title, _desc) {
+          var _contents = Array.from(this.state.contents);
+          var i = 0;
+          while (i < _contents.length) {
+            if (_contents[i].id === _id) {
+              _contents[i] = {id:_id, title:_title, desc:_desc};
+              break;
+            }
+            i = i + 1;
+          } 
+          this.setState({
+            contents: _contents,
+            mode: 'read'
+          });
+        }.bind(this)}></UpdateContent>;
+    }
+    return _article;
+  }
+
+  render() {
     return (
       <div className="App">
         <Subject 
@@ -53,8 +107,32 @@ class App extends Component {
             selected_content_id: Number(id)
           });
         }.bind(this)}
-        data={this.state.contents}></TOC>
-        <Content title={_title} desc={_desc}></Content>
+        data={this.state.contents}
+        ></TOC>
+        <Control onChangeMode={function(_mode) {
+          if (_mode === 'delete') {
+            if (window.confirm('Are you sure?')) {
+              let _contents = Array.from(this.state.contents);
+              let i = 0;
+              while (i < _contents.length) {
+                if (_contents[i].id === this.state.selected_content_id) {
+                  _contents.splice(i, 1);
+                }
+                i = i + 1;
+              }
+              this.setState({
+                mode: 'welcome',
+                contents:_contents
+              });
+              alert('completed');
+            }
+          } else {
+            this.setState({
+              mode:_mode
+            });
+          }
+        }.bind(this)}></Control>
+        {this.getContent()}
       </div>
     );
   }
